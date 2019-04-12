68869fc17b1d862f7925bde56ed94578e92486a9
Jashani <jasha99@g.co.il>
2019-04-12 16:10:30 +0300

    Added painting.

---
+++ Assets/TileScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour
+{
+
+    private Material material;
+    MeshRenderer meshRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        material = Resources.Load<Material>("PlayerMaterial");
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            meshRenderer.material = material;
+        }
+    }
+
+}
