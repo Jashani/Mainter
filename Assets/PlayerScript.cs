bcfd012374906668181884c6afad2ffe879a474c
Jashani <jasha99@g.co.il>
2019-04-12 12:30:56 +0300

    Movement is now locked until collision.

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -14,28 +14,34 @@ public class PlayerScript : MonoBehaviour
         Wait
     }
 
-    public float speed = 2f;
+    public float speed = 15f;
 
-    private State state = State.Wait;
+    private State state;
+    private bool lockActivity;
+    private float currentSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        state = State.Wait;
+        lockActivity = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("w"))
-            state = State.MoveNorth;
-        else if (Input.GetKeyDown("s"))
-            state = State.MoveSouth;
-        else if (Input.GetKeyDown("a"))
-            state = State.MoveWest;
-        else if (Input.GetKeyDown("d"))
-            state = State.MoveEast;
-
+        if (!lockActivity)
+        {
+            if (Input.GetKeyDown("w"))
+            { state = State.MoveNorth; lockActivity = true; }
+            else if (Input.GetKeyDown("s"))
+            { state = State.MoveSouth; lockActivity = true; }
+            else if (Input.GetKeyDown("a"))
+            { state = State.MoveWest; lockActivity = true; }
+            else if (Input.GetKeyDown("d"))
+            { state = State.MoveEast; lockActivity = true; }
+        }
+        
         if (state == State.MoveNorth)
             transform.position += Vector3.forward * speed * Time.deltaTime;
         else if(state == State.MoveSouth)
@@ -56,5 +62,6 @@ public class PlayerScript : MonoBehaviour
         Debug.Log("Collided.");
         state = State.Wait;
         transform.position = new Vector3(Mathf.Round(transform.position.x - 0.1f - fix) + 0.5f, transform.position.y, Mathf.Round(transform.position.z - 0.1f - fix) + 0.5f);
+        lockActivity = false;
     }
 }
